753804a773cf01a75b160131b260146baa96f5d8
vinothrao <vinothrao89@gmail.com>
2025-07-20 13:45:29 +0530

    fix

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,7 +2,7 @@
 
 "use client";
 
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import { useState } from "react";
 
 export default function Login() {
